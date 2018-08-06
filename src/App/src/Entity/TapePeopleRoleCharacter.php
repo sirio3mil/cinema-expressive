6dff4569c744fbcf43fc134b32457323188d46d1
Reynier de la Rosa <sirio3mil@gmail.com>
2018-08-06 21:13:49 +0200

    add annotations

---
+++ src/App/src/Entity/TapePeopleRoleCharacter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Annotation as ORM;
+
+/**
+ * Class PeopleDetail
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="TapePeopleRoleCharacter")
+ */
+class TapePeopleRoleCharacter implements CinemaEntity
+{
+
+    use CreationDate;
+
+    /**
+     * @var TapePeopleRole
+     *
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="TapePeopleRole", fetch="EXTRA_LAZY", orphanRemoval=false)
+     * @ORM\JoinColumn(name="tapePeopleRoleId", referencedColumnName="tapePeopleRoleId")
+     */
+    private $tapePeopleRole;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(
+     *     type="string",
+     *     length=300,
+     *     name="character",
+     *     nullable=false,
+     *     options={"fixed":false}
+     * )
+     */
+    private $character;
+
+    /**
+     * @param string $character
+     * @return TapePeopleRoleCharacter
+     */
+    public function setCharacter(string $character): TapePeopleRoleCharacter
+    {
+        $this->character = $character;
+    
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCharacter(): string
+    {
+        return $this->character;
+    }
+
+    /**
+     * @param TapePeopleRole $tapePeopleRole
+     * @return TapePeopleRoleCharacter
+     */
+    public function setTapePeopleRole(TapePeopleRole $tapePeopleRole): TapePeopleRoleCharacter
+    {
+        $this->tapePeopleRole = $tapePeopleRole;
+    
+        return $this;
+    }
+
+    /**
+     * @return TapePeopleRole
+     */
+    public function getTapePeopleRole(): TapePeopleRole
+    {
+        return $this->tapePeopleRole;
+    }
+}
