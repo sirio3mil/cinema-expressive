2c85aafcb948ba8b4b46e99aa0aac8e299ea1228
Reynier de la Rosa <sirio3mil@gmail.com>
2018-08-06 22:54:58 +0200

    add annotations

---
+++ src/App/src/Entity/TvShow.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Annotation as ORM;
+
+/**
+ * Class TvShow
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="TvShow")
+ */
+class TvShow implements CinemaEntity
+{
+
+    use CreationDate;
+
+    /**
+     * @var Tape
+     *
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="Tape", fetch="EXTRA_LAZY", orphanRemoval=false)
+     * @ORM\JoinColumn(name="tapeId", referencedColumnName="tapeId")
+     */
+    private $tape;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(
+     *     type="boolean",
+     *     name="finished",
+     *     nullable=false,
+     *     options={"default":0}
+     * )
+     */
+    private $finished;
+
+
+    /**
+     * @param bool $finished
+     * @return TvShow
+     */
+    public function setFinished(bool $finished): TvShow
+    {
+        $this->finished = $finished;
+    
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFinished(): bool
+    {
+        return $this->finished;
+    }
+
+    /**
+     * @param Tape $tape
+     * @return TvShow
+     */
+    public function setTape(Tape $tape): TvShow
+    {
+        $this->tape = $tape;
+    
+        return $this;
+    }
+
+    /**
+     * @return Tape
+     */
+    public function getTape(): Tape
+    {
+        return $this->tape;
+    }
+}
---
+++ src/App/src/Entity/TvShowChapter.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Annotation as ORM;
+
+/**
+ * Class TvShow
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="TvShow")
+ */
+class TvShowChapter implements CinemaEntity
+{
+
+    use CreationDate;
+
+    /**
+     * @var Tape
+     *
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="Tape", fetch="EXTRA_LAZY", orphanRemoval=false)
+     * @ORM\JoinColumn(name="tapeId", referencedColumnName="tapeId")
+     */
+    private $tape;
+
+    /**
+     * @@var TvShow
+     *
+     * @ORM\ManyToOne(targetEntity="TvShow", fetch="EXTRA_LAZY", orphanRemoval=false)
+     * @ORM\JoinColumn(name="tvShowTapeId", referencedColumnName="tapeId")
+     */
+    private $tvShow;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(
+     *     type="smallint",
+     *     name="season",
+     *     nullable=true,
+     *     options={"unsigned":false}
+     * )
+     */
+    private $season;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(
+     *     type="smallint",
+     *     name="chapter",
+     *     nullable=true,
+     *     options={"unsigned":false}
+     * )
+     */
+    private $chapter;
+
+
+    /**
+     * @param int|null $season
+     * @return TvShowChapter
+     */
+    public function setSeason(?int $season): TvShowChapter
+    {
+        $this->season = $season;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getSeason(): ?int
+    {
+        return $this->season;
+    }
+
+    /**
+     * @param int|null $chapter
+     * @return TvShowChapter
+     */
+    public function setChapter(?int $chapter): TvShowChapter
+    {
+        $this->chapter = $chapter;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChapter()
+    {
+        return $this->chapter;
+    }
+
+    /**
+     * @param Tape $tape
+     * @return TvShowChapter
+     */
+    public function setTape(Tape $tape): TvShowChapter
+    {
+        $this->tape = $tape;
+
+        return $this;
+    }
+
+    /**
+     * @return Tape
+     */
+    public function getTape(): Tape
+    {
+        return $this->tape;
+    }
+
+    /**
+     * @param TvShow $tvShow
+     * @return TvShowChapter
+     */
+    public function setTvShow(TvShow $tvShow): TvShowChapter
+    {
+        $this->tvShow = $tvShow;
+
+        return $this;
+    }
+
+    /**
+     * @return TvShow
+     */
+    public function getTvShow(): TvShow
+    {
+        return $this->tvShow;
+    }
+}
