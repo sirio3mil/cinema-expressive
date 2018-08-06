6c423acb5da9a730cf9272f8e41e9c3a03cac4d8
Reynier de la Rosa <sirio3mil@gmail.com>
2018-08-06 21:25:04 +0200

    add annotations

---
+++ src/App/src/Entity/TapePlot.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Annotation as ORM;
+
+/**
+ * Class Tape
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="TapePlot")
+ */
+class TapePlot implements CinemaEntity
+{
+
+    /**
+     * @var Tape
+     *
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="Tape", fetch="EXTRA_LAZY", orphanRemoval=false)
+     * @ORM\JoinColumn(name="tapeId", referencedColumnName="tapeId")
+     */
+    private $tape;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(
+     *     type="string",
+     *     length=0,
+     *     name="plot",
+     *     nullable=false,
+     *     options={"fixed":false}
+     * )
+     */
+    private $plot;
+
+
+    /**
+     * @param string $plot
+     * @return TapePlot
+     */
+    public function setPlot(string $plot): TapePlot
+    {
+        $this->plot = $plot;
+    
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlot(): string
+    {
+        return $this->plot;
+    }
+
+    /**
+     * @param Tape $tape
+     * @return TapePlot
+     */
+    public function setTape(Tape $tape): TapePlot
+    {
+        $this->tape = $tape;
+    
+        return $this;
+    }
+
+    /**
+     * @return Tape
+     */
+    public function getTape(): Tape
+    {
+        return $this->tape;
+    }
+}
