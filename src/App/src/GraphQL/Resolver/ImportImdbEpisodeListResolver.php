d3d5a1b8616e3d914202a4305edfd65f1a983992
Reynier de la Rosa <reynier.delarosa@digimobil.es>
2018-09-05 17:54:49 +0200

    add skeleton for new functionality

--- src/App/src/GraphQL/Resolver/ImportImdbEpisodeListResolver.php
+++ src/App/src/GraphQL/Resolver/ImportImdbEpisodeListResolver.php
@@ -10,11 +10,47 @@ namespace App\GraphQL\Resolver;
 
 
 use App\GraphQL\TypeRegistry;
+use GraphQL\Executor\ExecutionResult;
+use GraphQL\GraphQL;
+use Interop\Container\ContainerInterface;
+use MongoDB\Collection;
+use App\Alias\MongoDBClient;
 
 class ImportImdbEpisodeListResolver
 {
     public static function resolve(TypeRegistry $typeRegistry, array $args): array
     {
+        $source = CachedDocumentNodeResolver::resolve($typeRegistry->getCacheStorageAdapter(),
+            'queries/graphql/EpisodeList.graphql');
+        /** @var ExecutionResult $gqQueryResult */
+        $gqQueryResult = GraphQL::executeQuery(
+            $typeRegistry->getSchema(),
+            $source,
+            null,
+            null,
+            [
+                "imdbNumber" => $args['imdbNumber'],
+                "seasonNumber" => $args['seasonNumber']
+            ]
+        );
+        $date = new \DateTime();
+        /** @var ContainerInterface $container */
+        $container = $typeRegistry->getContainer();
+        $searchValues = [
+            "imdbNumber" => $args['imdbNumber'],
+            "seasonNumber" => $args['seasonNumber']
+        ];
+        /** @var Collection $collection */
+        $container->get(MongoDBClient::class)
+            ->cinema
+            ->episodes
+            ->findOneAndReplace(
+                $searchValues,
+                array_merge($gqQueryResult->data, $searchValues, ["updated" => $date]),
+                [
+                    "upsert" => true
+                ]
+            );
         return [
             'episodes' => [
                 [
