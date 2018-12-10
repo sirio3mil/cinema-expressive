7268fb8f085da19b6490db42e4e6df1ed7d913df
Reynier de la Rosa <reynier.delarosa@digimobil.es>
2018-12-10 16:32:30 +0100

    fix error 500

--- src/App/src/GraphQL/Resolver/MovieDetailResolver.php
+++ src/App/src/GraphQL/Resolver/MovieDetailResolver.php
@@ -9,32 +9,19 @@
 namespace App\GraphQL\Resolver;
 
 
-use App\GraphQL\TypeRegistry;
 use ImdbScraper\Mapper\HomeMapper;
 use Psr\Container\ContainerInterface;
-use Zend\Cache\Storage\Adapter\AbstractAdapter;
-use GraphQL\Type\Definition\Type;
 
 class MovieDetailResolver
 {
     
-    public function __construct(AbstractAdapter $cacheStorageAdapter, TypeRegistry $typeRegistry)
-    {
-        $this->setPageMapper(new HomeMapper());
-        $this->cacheStorageAdapter = $cacheStorageAdapter;
-        $this->type = $typeRegistry->get('movie');
-        $this->args = [
-            'imdbNumber' => Type::nonNull(Type::int()),
-        ];
-    }
-
     /**
      * @param ContainerInterface $container
      * @param array $args
      * @return array
      * @throws \Exception
      */
-    public function resolve(ContainerInterface $container, array $args): array
+    public static function resolve(ContainerInterface $container, array $args): array
     {
         /** @var HomeMapper $mapper */
         $mapper = $container->get(HomeMapper::class);
