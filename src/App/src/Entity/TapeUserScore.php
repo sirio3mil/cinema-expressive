fad1d320d1d7eadbc903bdefa342454dc9bb842d
Reynier de la Rosa <reynier.delarosa@digimobil.es>
2018-12-12 16:43:42 +0100

    add user data to search output

---
+++ src/App/src/Entity/TapeUserScore.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reynier.delarosa
+ * Date: 12/12/2018
+ * Time: 16:22
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class TapeUserScore
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="TapeUserScore")
+ * @ORM\HasLifecycleCallbacks
+ */
+class TapeUserScore implements CinemaEntity
+{
+
+    use CreationDate;
+
+    /**
+     * @var TapeUser
+     *
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="TapeUser", fetch="EXTRA_LAZY")
+     * @ORM\JoinColumn(name="tapeUserId", referencedColumnName="tapeUserId")
+     */
+    private $tapeUser;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(
+     *     type="boolean",
+     *     name="exported",
+     *     nullable=false,
+     *     options={"default":0}
+     * )
+     */
+    private $exported;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(
+     *     type="float",
+     *     name="score",
+     *     nullable=false,
+     *     options={"unsigned":false,"default":0}
+     * )
+     */
+    protected $score;
+
+    /**
+     * @param TapeUser $tapeUser
+     * @return TapeUserScore
+     */
+    public function setTapeUser(TapeUser $tapeUser): TapeUserScore
+    {
+        $this->tapeUser = $tapeUser;
+
+        return $this;
+    }
+
+    /**
+     * @return TapeUser
+     */
+    public function getTapeUser(): TapeUser
+    {
+        return $this->tapeUser;
+    }
+
+    /**
+     * @param float $score
+     * @return TapeUserScore
+     */
+    public function setScore(float $score): TapeUserScore
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getScore(): float
+    {
+        return $this->score ?? 0;
+    }
+
+    /**
+     * @param bool $exported
+     * @return TapeUserScore
+     */
+    public function setExported(bool $exported): TapeUserScore
+    {
+        $this->exported = $exported;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getExported(): bool
+    {
+        return $this->exported;
+    }
+
+    /** @ORM\PrePersist */
+    public function generateExportedFlag()
+    {
+        if(is_null($this->exported)) {
+            $this->setExported(false);
+        }
+    }
+
+}
--- src/App/src/GraphQL/Resolver/SearchResolver.php
+++ src/App/src/GraphQL/Resolver/SearchResolver.php
@@ -14,6 +14,11 @@ use App\Entity\ImdbNumber;
 use App\Entity\People;
 use App\Entity\RowType;
 use App\Entity\Tape;
+use App\Entity\TapeUser;
+use App\Entity\TapeUserHistory;
+use App\Entity\TapeUserHistoryDetail;
+use App\Entity\TapeUserScore;
+use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
 use Zend\Db\Adapter\Adapter;
@@ -28,11 +33,19 @@ class SearchResolver
      */
     public static function resolve(ContainerInterface $container, array $args): array
     {
-
         /** @var Adapter $adapter */
         $adapter = $container->get(Adapter::class);
         /** @var EntityManager $entityManager */
         $entityManager = $container->get(EntityManager::class);
+
+        $userLogged = filter_var($args['userId'], FILTER_VALIDATE_INT);
+        /** @var User|null $user */
+        $user = null;
+        if ($userLogged){
+            $user = $entityManager->getRepository(User::class)->findOneBy([
+                "userId" => $args['userId']
+            ]);
+        }
 
         $sql = "exec dbo.SearchParam ?";
         /** @var ResultSet $stmt */
@@ -55,6 +68,7 @@ class SearchResolver
                 "object" => $object
             ]);
             $original = null;
+            $userObject = [];
             switch ($rowTypeId) {
                 case RowType::ROW_TYPE_PEOPLE:
                     /** @var People $person */
@@ -71,6 +85,48 @@ class SearchResolver
                     ]);
                     $internalId = $tape->getTapeId();
                     $original = $tape->getOriginalTitle();
+                    if($user){
+                        /** @var TapeUser $tapeUser */
+                        $tapeUser = $entityManager->getRepository(TapeUser::class)->findOneBy([
+                            "user" => $user,
+                            "tape" => $tape
+                        ]);
+                        if($tapeUser) {
+                            $userObject['objectUserId'] = $tapeUser->getTapeUserId();
+                            /** @var TapeUserScore $tapeScore */
+                            $tapeScore = $entityManager->getRepository(TapeUserScore::class)->findOneBy([
+                                "tapeUser" => $tapeUser
+                            ]);
+                            if($tapeScore){
+                                $userObject['score'] = $tapeScore->getScore();
+                                $userObject['scoreDate'] = $tapeScore->getCreatedAt()->format("d/m/Y");
+                            }
+                            /** @var TapeUserHistory[] $tapeUserHistories */
+                            $tapeUserHistories = $entityManager->getRepository(TapeUserHistory::class)->findBy([
+                                "tapeUser" => $tapeUser
+                            ]);
+                            if($tapeUserHistories){
+                                $histories = [];
+                                /** @var TapeUserHistory $tapeUserHistory */
+                                foreach ($tapeUserHistories as $tapeUserHistory){
+                                    $histories['statusId'] = $tapeUserHistory->getTapeUserStatus()->getTapeUserStatusId();
+                                    $histories['status'] = $tapeUserHistory->getTapeUserStatus()->getStatusDescription();
+                                    $details = [];
+                                    /** @var TapeUserHistoryDetail $tapeUserHistoryDetail */
+                                    $tapeUserHistoryDetail = $entityManager->getRepository(TapeUserHistoryDetail::class)->findOneBy([
+                                        "tapeUserHistory" => $tapeUserHistory
+                                    ]);
+                                    $details['date'] = $tapeUserHistoryDetail->getCreatedAt()->format("d/m/Y");
+                                    $details['placeId'] = $tapeUserHistoryDetail->getPlace()->getPlaceId();
+                                    $details['visible'] = $tapeUserHistoryDetail->getVisible();
+                                    $details['exported'] = $tapeUserHistoryDetail->getExported();
+                                    $details['place'] = $tapeUserHistoryDetail->getPlace()->getDescription();
+                                    $histories['details'] = $details;
+                                }
+                                $userObject['history'] = $histories;
+                            }
+                        }
+                    }
                     break;
             }
 
@@ -81,7 +137,8 @@ class SearchResolver
                 'rowType' => $rowType->getDescription(),
                 'internalId' => $internalId,
                 'imdbNumber' => $imdbNumber ? $imdbNumber->getImdbNumber() : 0,
-                'original' => $original
+                'original' => $original,
+                'userObject' => $userObject
             ];
         }
 
