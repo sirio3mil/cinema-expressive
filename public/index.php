fcea0df371bf3144527efab62d34fe9cb202e71e
Reynier de la Rosa <reynier.delarosa@parclick.com>
2019-08-08 15:48:11 +0200

    add namespaces

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
-// Delegate static file requests back to the PHP built-in webserver
+use Psr\Container\ContainerInterface;
+use Zend\Expressive\Application;
+use Zend\Expressive\MiddlewareFactory;
+
+// Delegate static file requests back to the PHP built-in web server
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
@@ -14,12 +18,12 @@ require 'vendor/autoload.php';
  * Self-called anonymous function that creates its own scope and keep the global namespace clean.
  */
 (function () {
-    /** @var \Psr\Container\ContainerInterface $container */
+    /** @var ContainerInterface $container */
     $container = require 'config/container.php';
 
-    /** @var \Zend\Expressive\Application $app */
-    $app = $container->get(\Zend\Expressive\Application::class);
-    $factory = $container->get(\Zend\Expressive\MiddlewareFactory::class);
+    /** @var Application $app */
+    $app = $container->get(Application::class);
+    $factory = $container->get(MiddlewareFactory::class);
 
     // Execute programmatic/declarative middleware pipeline and routing
     // configuration statements
