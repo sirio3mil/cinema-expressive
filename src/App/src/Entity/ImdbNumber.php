897a35c89a71d33f76f1ae84a1ebcfa21ec30dcf
Reynier de la Rosa <reynier.delarosa@parclick.com>
2019-08-08 11:59:46 +0200

    add url generator getter

--- src/App/src/Entity/ImdbNumber.php
+++ src/App/src/Entity/ImdbNumber.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use function str_pad;
 
 /**
  * Class ImdbNumber
@@ -43,7 +44,7 @@ class ImdbNumber implements CinemaEntity
     public function setImdbNumber(int $imdbNumber): ImdbNumber
     {
         $this->imdbNumber = $imdbNumber;
-    
+
         return $this;
     }
 
@@ -53,5 +54,23 @@ class ImdbNumber implements CinemaEntity
     public function getImdbNumber(): int
     {
         return $this->imdbNumber;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrl(): ?string
+    {
+        $formattedNumber = str_pad($this->imdbNumber, 7, "0", STR_PAD_LEFT);
+        $rowType = $this->object->getRowType();
+
+        switch ($rowType->getRowTypeId()) {
+            case RowType::ROW_TYPE_TAPE:
+                return "https://www.imdb.com/title/tt{$formattedNumber}/";
+            case RowType::ROW_TYPE_PEOPLE:
+                return "https://www.imdb.com/name/nm{$formattedNumber}/";
+        }
+
+        return null;
     }
 }
